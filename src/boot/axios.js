9216725508ac4b6fd7b816625e6edc68d2ed9be4
Kizzuato <kyyajuara@gmail.com>
2024-03-01 18:13:54 +0700

    changing api to local

--- src/boot/axios.js
+++ src/boot/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 const axiosInstance = axios.create({
-  baseURL: 'https://api-prmn.curaweda.com:3000'
-  // baseURL: 'http://localhost:3000'
+  // baseURL: 'https://api-prmn.curaweda.com:3000'
+  baseURL: 'http://localhost:3000'
 })
 export default ({ Vue }) => {
   Vue.prototype.$axios = axiosInstance
