9ad8f268582d442d0393704de8031f61ad7e80cc
Harmoni Natanael S <harmoninatanaels@gmail.com>
2024-03-13 11:35:36 +0700

    daidhioashdoi

--- src/boot/axios.js
+++ src/boot/axios.js
@@ -1,8 +1,8 @@
 import axios from "axios";
 const axiosInstance = axios.create({
-  baseURL: 'https://api-prmn.curaweda.com:3000'
+  // baseURL: 'https://api-prmn.curaweda.com:3000'
   // baseURL: 'https://wg3bg68h-3000.asse.devtunnels.ms'
-  // baseURL: 'http://localhost:3000'
+  baseURL: 'http://localhost:3000'
 })
 export default ({ Vue }) => {
   Vue.prototype.$axios = axiosInstance;
