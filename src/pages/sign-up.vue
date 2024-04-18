d33c2add39c1a5040977f63a12775fd8a7a5e2d9
Razan Muhammad <razanmfs507@gmail.com>
2024-04-18 14:14:29 +0700

    /dskajlkdas

--- src/pages/sign-up.vue
+++ src/pages/sign-up.vue
@@ -41,7 +41,7 @@
                     <!-- button section -->
 
                     <!-- form section -->
-                    <q-form class="q-gutter-md" @submit="SignIn()">
+                    <q-form class="q-gutter-md" @submit="SignUp()">
                       <q-select
                         filled
                         label="Cabang"
@@ -117,8 +117,6 @@
                     <!-- form section -->
                   </div>
                 </div>
-
-                <q-section></q-section>
 
                 <!-- gambar makanan -->
                 <div class="col-md-6 col-xs-12 q-ml-xl desktop-only">
@@ -239,77 +237,14 @@ export default {
       }
     },
 
-    async SignIn() {
+    //function signup
+    async SignUp() {
       const email = Cookies.get("email");
       const data = {
         email: this.email ? this.email : email,
         password: this.password || null,
         branch: this.branch ? this.branch.value : null,
       };
-      console.log("🚀 ~ SignIn ~ data.token:", data.token);
-
-      try {
-        const token = Cookies.get("token");
-        console.log("🚀 ~ SignIn ~ token:", token);
-        const response = await this.$axios.post("/user/login", data, {
-          headers: {
-            "Content-Type": "application/json",
-          },
-          params: {
-            token: token ? token : null,
-          },
-        });
-
-        if (response.status === 200) {
-          const accessToken = response.data.data.accessToken;
-          const email = response.data.data.email;
-          const id = response.data.data.id;
-          const name = response.data.data.name;
-          const title = response.data.data.title;
-          const division = response.data.data.division;
-          const branch = response.data.data.branch;
-          const division_id = response.data.data.division_id;
-          const branch_id = response.data.data.branch_id;
-
-          if (this.right) {
-            Cookies.set("token", accessToken, { expires: 365 }); // Cookie berlaku selama 1 tahun
-            Cookies.set("id", id, { expires: 365 });
-            Cookies.set("email", email, { expires: 365 });
-            Cookies.set("username", name, { expires: 365 });
-            Cookies.set("title", title, { expires: 365 });
-            Cookies.set("division", division, { expires: 365 });
-            Cookies.set("branch", branch, { expires: 365 });
-            Cookies.set("division_id", division_id, { expires: 365 });
-            Cookies.set("branch_id", branch_id, { expires: 365 });
-          } else {
-            sessionStorage.setItem("token", accessToken);
-            sessionStorage.setItem("id", id);
-            sessionStorage.setItem("email", email);
-            sessionStorage.setItem("username", name);
-            sessionStorage.setItem("title", title);
-            sessionStorage.setItem("division", division);
-            sessionStorage.setItem("branch", branch);
-            sessionStorage.setItem("division_id", division_id);
-            sessionStorage.setItem("branch_id", branch_id);
-          }
-          this.redirectUser(title);
-
-          this.$q.notify({
-            color: "positive",
-            message: "Login Successful.",
-          });
-        } else {
-          throw new Error("Invalid email or password");
-        }
-      } catch (error) {
-        console.error("Error signing in:", error);
-
-        this.$q.notify({
-          color: "negative",
-          position: "top",
-          message: "Invalid email or password",
-        });
-      }
     },
 
     redirectUser: function (title) {
